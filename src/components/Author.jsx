3e3a6e5393c08e13a0d7e96f370680483aaf5fa7
Manoj Bhat <bahatanm@gmail.com>
2020-04-01 22:15:55 +0530

    Added social media details

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import manoj from "../imgs/ManojBhat.jpg";
+import git from "../imgs/git.svg";
+import lnkIn from "../imgs/linkedin.svg";
+import so from "../imgs/stack-overflow.svg";
 
 const Author = () => {
   return (
     <div className="author">
-      <img src={manoj} alt="Manoj Bhat" />
+      <img src={manoj} alt="Manoj Bhat" className="author-img" />
       <div className="details">
         <h4>Manoj Bhat</h4>
         <p>Software engineer, Math and physics enthusiast</p>
+        <div className="icons">
+          <a href="https://github.com/Sigkill32" target="_blank">
+            <img src={git} alt="Github" />
+          </a>
+          <a
+            href="https://stackoverflow.com/users/12893110/manoj-bhat"
+            target="_blank"
+          >
+            <img src={so} alt="Stack overflow" />
+          </a>
+          <a href="https://www.linkedin.com/in/manoj-bhat-an/" target="_blank">
+            <img src={lnkIn} alt="LinkedIn" />
+          </a>
+        </div>
       </div>
     </div>
   );
